82eca97f28617c798fccf8302f8291b037379895
MyLadySnezhka <mylady.snezhka@gmail.com>
2023-01-27 11:57:11 +0200

    finish modal window & shadow

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
-const elBtn = document.querySelector('button[name=btn1]');
+const elBtn = document.querySelector('.cart_button');
 const elRoot = document.querySelector('.root');
 const elModal = document.querySelector('.modal');
+const elBtnClose = document.querySelector('button[name=btn_close]');
+const elBtnOK = document.querySelector('button[name=btn_OK]');
 
 elBtn.addEventListener('click', () => {
     elModal.classList.add('show');
-    elRoot.classList.add('show');
-})
+    elRoot.classList.add('shadow');
+});
+
+const btn_close = () => {
+    elModal.classList.remove('show');
+    elRoot.classList.remove('shadow');
+    return;
+};
+
+elBtnClose.addEventListener('click', () => {
+    btn_close();
+});
+
+elBtnOK.addEventListener('click', () => {
+    btn_close();
+});
 
